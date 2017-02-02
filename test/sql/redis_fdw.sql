f08eebf845821d61af5da2aa8f67a1711f3e67ac
Andrew Dunstan <andrew@dunslane.net>
2017-02-02 03:21:59 -0500

    Revert "comply with upstream's stricter psql variable setting syntax"
    This reverts commit a6ad9342ea8f919fd84f39de5e6c5a3604872d7f.
    
    Upstream has adjusted to allow legacy behaviour

--- test/sql/redis_fdw.sql
+++ test/sql/redis_fdw.sql
@@ -39,7 +39,7 @@ create foreign table db15_zset(key text, value text)
 
 -- make sure they are all empty - if any are not stop the script right now
 
-\set ON_ERROR_STOP on
+\set ON_ERROR_STOP
 do $$
   declare
     rows bigint;

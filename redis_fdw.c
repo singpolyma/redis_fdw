15a19aeebe0efd32f9838c5619ec91e8ae1e0732
Andrew Dunstan <andrew@dunslane.net>
2021-06-28 17:46:33 -0400

    adjust for REL_14_STABLE

--- redis_fdw.c
+++ redis_fdw.c
@@ -22,8 +22,8 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 140000
-#error wrong Postgresql version this branch is only for 14.
+#if PG_VERSION_NUM < 150000
+#error wrong Postgresql version this branch is only for 15.
 #endif
 
 
